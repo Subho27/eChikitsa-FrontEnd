524bd2b36c03afa71d40de59ab5a15d4357cf427
Subho27 <42699151+Subho27@users.noreply.github.com>
2024-01-16 22:30:36 +0530

    Routing enabled and added initial components

---
+++ echikitsa/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home ( props ) {
+    return (
+        <div>
+            <p>Hello from our application!!</p>
+        </div>
+    );
+}
+
+export default Home;
