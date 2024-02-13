c4d7663523918abbae853cf96a219d92b49e78ea
ajay-gidd <ajay.a.gidd@gmail.com>
2024-02-13 23:38:22 +0530

    Landing Page, Patient Landing page

--- echikitsa/src/App.js
+++ echikitsa/src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
+import Patient_landing from "./components/helper-components/patient_landing";
+import Landing from "./components/helper-components/landing_body"
+import Hospital from "./components/helper-components/Hospital";
 
 function App() {
   return (
       <Routes>
-          <Route path='/' element={ <Home/> } />
-          <Route path='/signup' element={ <SignUp/> } />
+          <Route path='/' element={ <Landing/> } />
+          <Route path='/pateint' element={ <Patient_landing/> } />
+          <Route path='/hospital' element={ <Hospital/> } />
+          {/*<Route path='/signup' element={ <SignUp/> } />*/}
       </Routes>
   );
 }
