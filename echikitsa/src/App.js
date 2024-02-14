f4ce18b6b3f0d5ac1185ac6d3a5178f6cae95fd0
Rishav Chandel <rishavchandel456@gmail.com>
2024-02-14 19:45:00 +0530

    login page created

--- echikitsa/src/App.js
+++ echikitsa/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
-import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import Patient_landing from "./components/helper-components/patient_landing";
 import Landing from "./components/helper-components/landing_body"
 import Hospital from "./components/helper-components/Hospital";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -12,7 +12,8 @@ function App() {
           <Route path='/' element={ <Landing/> } />
           <Route path='/pateint' element={ <Patient_landing/> } />
           <Route path='/hospital' element={ <Hospital/> } />
-          {/*<Route path='/signup' element={ <SignUp/> } />*/}
+          <Route path='/login' element={ <Login/> } />
+          <Route path='/signup' element={ <SignUp/> } />
       </Routes>
   );
 }
