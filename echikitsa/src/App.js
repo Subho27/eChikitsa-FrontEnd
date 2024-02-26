a0fb690845806d383987cfc56320a3fedb6a2f9a
Rishav Chandel <rishavchandel456@gmail.com>
2024-02-26 22:48:05 +0530

    Added Doctor record and monitor page

--- echikitsa/src/App.js
+++ echikitsa/src/App.js
@@ -4,6 +4,9 @@ import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import Patient_landing from "./components/helper-components/patient_landing";
 import Login from "./components/Login";
+import DocProfilePage from "./components/helper-components/helper-doctor/DocProfileHelper";
+import DocRecordPage from "./components/helper-components/helper-doctor/DocRecordHelper";
+import DocMonitorPage from "./components/helper-components/helper-doctor/DocMonitorHelper";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
           <Route path='/signup' element={ <SignUp/> } />
           <Route path='/login' element={ <Login/> } />
           <Route path='/pateint' element={ <Patient_landing/> } />
+          <Route path='/doctorprofile' element={ <DocProfilePage/> } />
+          <Route path='/doctorrecord' element={ <DocRecordPage/>} />
+          <Route path='/doctormonitor' element={ <DocMonitorPage/>} />
       </Routes>
   );
 }
