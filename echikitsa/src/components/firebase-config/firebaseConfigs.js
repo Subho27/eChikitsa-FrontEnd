ce439ff3c1e6b3043583a01ce76b39c16e17a3f3
Subho27 <42699151+Subho27@users.noreply.github.com>
2024-03-28 23:30:05 +0530

    Mobile otp verification with firebase

---
+++ echikitsa/src/components/firebase-config/firebaseConfigs.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDwzwWNy1tobd0RTnrNUqjfVyVOU3-FqlE",
+    authDomain: "echikitsa-b4fc8.firebaseapp.com",
+    databaseURL: "https://echikitsa-b4fc8-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "echikitsa-b4fc8",
+    storageBucket: "echikitsa-b4fc8.appspot.com",
+    messagingSenderId: "254572421559",
+    appId: "1:254572421559:web:f53a89bb97e76a1f038832",
+    measurementId: "G-0T5936KCPF"
+};
