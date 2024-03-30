d3989eb26499f645a2509e994628a95d7e9a5aa8
ajay-gidd <155996719+ajay-gidd@users.noreply.github.com>
2024-03-30 16:58:18 +0530

    Update firebaseConfigs.js

--- echikitsa/src/components/firebase-config/firebaseConfigs.js
+++ echikitsa/src/components/firebase-config/firebaseConfigs.js
@@ -8,13 +8,3 @@
 //     appId: ${{ secrets.APP_ID }},
 //     measurementId: ${{ secrets.MEASUREMENT_ID }}
 // };
-export const firebaseConfig = {
-    apiKey: "AIzaSyDwzwWNy1tobd0RTnrNUqjfVyVOU3-FqlE",
-    authDomain: "echikitsa-b4fc8.firebaseapp.com",
-    databaseURL: "https://echikitsa-b4fc8-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "echikitsa-b4fc8",
-    storageBucket: "echikitsa-b4fc8.appspot.com",
-    messagingSenderId: "254572421559",
-    appId: "1:254572421559:web:f53a89bb97e76a1f038832",
-    measurementId: "G-0T5936KCPF"
-};
